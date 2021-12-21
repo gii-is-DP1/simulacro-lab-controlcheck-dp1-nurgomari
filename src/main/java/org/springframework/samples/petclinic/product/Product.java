ac0f0410d4845c61d1ba491b2884b054b3c1f35f
Nuria <nurgomari@alum.us.es>
2021-12-21 12:08:37 +0100

    simulacro

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,22 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+public class Product extends NamedEntity{
+    //@Length(min=3,max=50)
+    //String name;
+	@Min(0)
     double price;
+	@ManyToOne
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,17 +2,24 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+	@Autowired
+	ProductRepository pr;
+	
     public List<Product> getAllProducts(){
-        return null;
+        return pr.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return pr.getProductsCheaperThan(price);
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return pr.getProductType(typeName);
     }
 
     public Product save(Product p){
